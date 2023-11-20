9eea580bc812d0df4783ccecd9bbb79104a0d463
ilaydadaban00 <145451273+ilaydadaban00@users.noreply.github.com>
2023-11-20 10:58:39 +0300

    Add files via upload

---
+++ lblCiftleri karsıya at/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace lblCiftleri_karsıya_at
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSayilar = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtAranan = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblSayilar
+            // 
+            this.lblSayilar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.lblSayilar.FormattingEnabled = true;
+            this.lblSayilar.Location = new System.Drawing.Point(77, 106);
+            this.lblSayilar.Name = "lblSayilar";
+            this.lblSayilar.Size = new System.Drawing.Size(169, 264);
+            this.lblSayilar.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(62, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // txtAranan
+            // 
+            this.txtAranan.Location = new System.Drawing.Point(164, 57);
+            this.txtAranan.Name = "txtAranan";
+            this.txtAranan.Size = new System.Drawing.Size(100, 20);
+            this.txtAranan.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(416, 450);
+            this.Controls.Add(this.txtAranan);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblSayilar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lblSayilar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtAranan;
+    }
+}
+
---
+++ lblCiftleri karsıya at/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lblCiftleri_karsıya_at
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int aranan=Convert.ToInt32(txtAranan.Text);
+            bool bulundu=false;
+
+
+            for(int i = 0; i < 20; i++)
+            {
+                int sayi = Convert.ToInt32(lblSayilar.Items[i]);
+                if (sayi == aranan)
+                {
+                    bulundu = true;
+                }  break;
+            }
+
+            if (bulundu)
+            {
+                MessageBox.Show("bulundu");
+            }
+            else
+            {
+                MessageBox.Show("bulunmadı");
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random random = new Random();
+           for(int i = 0; i < 20; i++)
+            {
+                lblSayilar.Items.Add(random.Next(1, 101));
+            }
+        }
+    }
+}
